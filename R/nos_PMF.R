f752c715068384da6d0d33d32935cc2b686ab134
ButterickJoe <135118165+ButterickJoe@users.noreply.github.com>
2025-06-26 16:04:20 +0100

    making nieces go faster

--- R/nos_PMF.R
+++ R/nos_PMF.R
@@ -1,4 +1,5 @@
 
+
 #' Title Unconditional PDF for nieces through Focal's older sisters
 #' @param y age of Focal
 #' @param s2 age of older niece when Focal is y
@@ -12,19 +13,35 @@ nos_PMF  <- function(y, s2, U_mat, F_mat, Q){
   probable_ages_of_mothering <- sapply(1:ncol(F_mat), function(x) mothers_age(U_mat, F_mat, x))
   actual_ages_of_mothering <- which(probable_ages_of_mothering != 0) - 1
   U_prob_niece <- U_kin_death(0, s2-1, Q, U_mat)
+  age_dists <- fert_dists(F_mat, Q)
+  Q_dists <- lapply(actual_ages_of_mothering, function(x){Q_matrix(x, Q, F_mat)})
+  rho_probs <- probable_ages_of_mothering[(actual_ages_of_mothering+1)]
+  rho_probs_mat <- rep(1, Q) %*% matrix(rho_probs, nrow = 1)
 
-  niece_list <- list()
-  for(s1 in (y + 1) : max(actual_ages_of_mothering)){
-    age_OS_at_Niece <- s1 - s2
-    age_foc_when_Niece_made <- y - s2
-    if(age_OS_at_Niece %in% actual_ages_of_mothering){
-      os_pdf <- os_PMF(age_foc_when_Niece_made, age_OS_at_Niece, U_mat, F_mat, Q)
-      QQ <- Q_matrix(age_OS_at_Niece, Q, F_mat)
-      newborn_niece <- QQ %*% os_pdf
-      nieces_f_ <- U_prob_niece %*% newborn_niece
-      niece_list[[(1+length(niece_list))]] <- nieces_f_}}
-
-  if(length(niece_list)>0){prob <- convoluion_nth(length(niece_list), niece_list)}
-  else{prob <- c(1, seq(0,Q-1))}
+  result_list <- lapply(actual_ages_of_mothering, function(OS_AT_NOS) {
+    b1 <- actual_ages_of_mothering
+    mum_age_at_os <- b1 + y - (OS_AT_NOS + s2) # age of mum when having older sis
+    index_list <- which((y < OS_AT_NOS + s2) & (mum_age_at_os %in% actual_ages_of_mothering)) ## ages of gran at which she can have aunt, such that aunt is younger then mother
+    # Conditional reproductive pmfs of mum (for element-wise product through resp. probs in "rho_probs_mat")
+    dist_mat_sis <- matrix(0, nrow = Q, ncol = length(actual_ages_of_mothering))
+    dist_mat_sis[1, ] <- 1  # by default we assume no reproduction of newborns unless otherwise
+    # If condition_T is met
+    if (length(index_list) > 0) { # if otherwise
+      U_prob_sis <- U_kin_death(0, OS_AT_NOS - 1, Q, U_mat) # prob sis survives from birth to age when she has niece
+      for (i in index_list) {
+        mum_age_os <- mum_age_at_os[i]
+        older_sis_newborn <- age_dists[[(1+mum_age_os)]]
+        dist_mat_sis[, i] <- U_prob_sis %*% older_sis_newborn ## pmf of these aunts when they have cousin
+      }
+    }
+    # Projected PMF for grandmother's reproduction
+    OS_pfm_at_niece <- (rho_probs_mat * dist_mat_sis) %*% rep(1, ncol(dist_mat_sis)) ## pmf of aunts at cousin using the probabilistic sum over b1, b2
+    Q_OS <- Q_dists[[(OS_AT_NOS+1-actual_ages_of_mothering[1])]] # reproduction of aunt
+    newborn_NOS <- Q_OS %*% OS_pfm_at_niece ## --> newborn cousins
+    suv_NOS <- U_prob_niece %*% newborn_NOS ## Cousins who suvive to age s2 when Focal is y
+    return(suv_NOS)
+  })
+  # Convolution of distributions over all ages of aunt s1 in actual_ages_of_mothering, under the constraint b1 + y > (YA_age_CYA + s2)
+  prob <- convoluion_nth(length(result_list), result_list)
   return(prob)
 }
--- R/nys_PMF.R
+++ R/nys_PMF.R
@@ -2,6 +2,7 @@
 
 
 
+
 #' Title Unconditional PDF for nieces through Focal's younger sisters
 #' @param y age of Focal
 #' @param s2 age of younger niece when Focal is y
@@ -13,22 +14,41 @@
 
 nys_PMF  <- function(y, s2, U_mat, F_mat, Q){
 
+  mother_vec <- e_vector(2, Q)
   probable_ages_of_mothering <- sapply(1:ncol(F_mat), function(x) mothers_age(U_mat, F_mat, x))
   actual_ages_of_mothering <- which(probable_ages_of_mothering != 0) - 1
   U_prob_niece <- U_kin_death(0, s2-1, Q, U_mat)
+  Q_dists <- lapply(actual_ages_of_mothering, function(x){Q_matrix(x, Q, F_mat)})
+  rho_probs <- probable_ages_of_mothering[(actual_ages_of_mothering+1)]
+  rho_probs_mat <- rep(1, Q) %*% matrix(rho_probs, nrow = 1)
 
-  niece_list <- list()
-  for(s1 in 0 : (y - 1)){
-    age_YS_at_Niece <- s1 - s2
-    age_foc_when_Niece_made <- y - s2
-    if(age_YS_at_Niece %in% actual_ages_of_mothering){
-      ys_pdf <- ys_PMF(age_foc_when_Niece_made, age_YS_at_Niece, U_mat, F_mat, Q)
-      QQ <- Q_matrix(age_YS_at_Niece, Q, F_mat)
-      newborn_niece <- QQ %*% ys_pdf
-      nieces_f_ <- U_prob_niece %*% newborn_niece
-      niece_list[[(1+length(niece_list))]] <- nieces_f_}}
-
-  if(length(niece_list)>0){prob <- convoluion_nth(length(niece_list), niece_list)}
-  else{prob <- c(1, rep(0, Q-1))}
+  result_list <- lapply(actual_ages_of_mothering, function(YS_AT_NYS) {
+    b1 <- actual_ages_of_mothering
+    mum_age_at_ys <- b1 + y - (YS_AT_NYS + s2) # age of mum when having younger sis
+    index_list <- which((y > YS_AT_NYS + s2) & (mum_age_at_ys %in% actual_ages_of_mothering)) ## ages of gran at which she can have aunt, such that aunt is younger then mother
+    # Conditional reproductive pmfs of gran (for element-wise product through resp. probs in "rho_probs_mat")
+    dist_mat_sis <- matrix(0, nrow = Q, ncol = length(actual_ages_of_mothering))
+    dist_mat_sis[1, ] <- 1  # by default we assume no reproduction of newborns unless otherwise
+    # If condition_T is met
+    if (length(index_list) > 0) { # if otherwise
+      U_prob_sis <- U_kin_death(0, YS_AT_NYS - 1, Q, U_mat) # prob sis survives from birth to age when she has niece
+      for (i in index_list) {
+        mum_age_ys <- mum_age_at_ys[i]
+        U_prob_mum <- U_kin_death(b1[i], mum_age_ys - 1, Q, U_mat)
+        pmf_mum_ys <- U_prob_mum %*% mother_vec # pmf of gran (Bernoulli) when having aunt
+        Q_mum <- Q_dists[[(mum_age_ys+1-actual_ages_of_mothering[1])]] ## reproduction pmf at this age
+        younger_sis_newborn <- Q_mum %*% pmf_mum_ys ## pmf of newborn aunts
+        dist_mat_sis[, i] <- U_prob_sis %*% younger_sis_newborn ## pmf of these aunts when they have cousin
+      }
+    }
+    # Projected PMF for grandmother's reproduction
+    YS_pfm_at_niece <- (rho_probs_mat * dist_mat_sis) %*% rep(1, ncol(dist_mat_sis)) ## pmf of aunts at cousin using the probabilistic sum over b1, b2
+    Q_YS <- Q_dists[[(YS_AT_NYS+1-actual_ages_of_mothering[1])]] # reproduction of aunt
+    newborn_NYS <- Q_YS %*% YS_pfm_at_niece ## --> newborn cousins
+    suv_NYS <- U_prob_niece %*% newborn_NYS ## Cousins who suvive to age s2 when Focal is y
+    return(suv_NYS)
+  })
+  # Convolution of distributions over all ages of aunt s1 in actual_ages_of_mothering, under the constraint b1 + y > (YA_age_CYA + s2)
+  prob <- convoluion_nth(length(result_list), result_list)
   return(prob)
 }
